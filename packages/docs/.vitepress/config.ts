45868eb28e240870d38d5e2a805c66d53259f98b
cloud-mouse <>
2024-10-08 10:38:10 +0800

    fix: name

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ import {
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'Hao-UI',
+  title: 'Z-Zimo-UI',
   description: '基于Vue3 高仿 element-ui 组件库',
   appearance: false, // 关闭 darkMode @todo 深色模式完成后打开
   base: '/z-zimo-ui/',
--- packages/docs/index.md
+++ packages/docs/index.md
@@ -3,7 +3,7 @@
 layout: home
 
 hero:
-  name: Hao-UI 组件库
+  name: Z-Zimo-UI 组件库
   text: 仿 ElementPlus
   tagline: 基于 Vue3 + TS 开发的组件库
   actions:
